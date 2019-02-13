93c21bd198fae3a38c4c962cb73aaef724540657
tinle <tinleduc92@gmail.com>
2019-02-13 15:10:42 +0700

    them login_new

--- apps/cart/views.py
+++ apps/cart/views.py
@@ -3,6 +3,18 @@ from django.views import View
 from django.http import HttpResponse
 # Create your views here.
 
+class Login(View):
+
+    def get(self, request):
+        return render(request, 'login_new.html')
+
+    def post(self, request):
+        print(request.POST['username'])
+        print(request.POST['password'])
+        if request.POST['username'] == 'admin' and request.POST['password']  == '123':
+        	return HttpResponse('Dang nhap thanh cong')
+        else:
+        	return HttpResponse ('Sai thong tin Ten dang nhap hoac Mat khau')
 
 class CartView(View):
 
